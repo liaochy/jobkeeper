b4f0ec3bae1b9066754e755af480ed4f46cd15e9
cyril <changyuliao@sohu-inc.com>
2013-12-16 11:10:23 +0800

    添加开机自启动

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+###################
+. /etc/profile
+. ~/.bash_profile
+##################
+
 bin=`dirname "$0"`
 JOBKEEPER_HOME=`cd "$bin"; pwd -P`
-
 cd $JOBKEEPER_HOME
 
 LIB_DIR=${JOBKEEPER_HOME}/lib
@@ -23,11 +27,32 @@ done
 if [ -f "${JOBKEEPER_HOME}/env.sh" ]; then
   . "${JOBKEEPER_HOME}/env.sh"
 fi
+
+# Attempt to set JAVA_HOME if it is not set
+if [[ -z $JAVA_HOME ]]; then
+  # On OSX use java_home (or /Library for older versions)
+  if [ "Darwin" == "$(uname -s)" ]; then
+    if [ -x /usr/libexec/java_home ]; then
+      export JAVA_HOME=($(/usr/libexec/java_home))
+    else
+      export JAVA_HOME=(/Library/Java/Home)
+    fi
+  fi
+
+  # Bail if we did not detect it
+  if [[ -z $JAVA_HOME ]]; then
+    echo "Error: JAVA_HOME is not set and could not be found." 1>&2
+    exit 1
+  fi
+fi
+
+JAVA=$JAVA_HOME/bin/java
+
 OLD=`ps auxf | grep "${MAIN_CLASS}" | grep ${JOBKEEPER_ID}|grep -v "grep"| awk '{print $2}'`
 if [ x$OLD = x ] 
   then
   	  touch "${LOGS_DIR}/sys.log"
-      nohup nice -n $JOBKEEPER_NICENESS  java ${JAVA_ARGS} -classpath $CLASSPATH ${MAIN_CLASS} ${JOBKEEPER_ID}  $@>>${LOGS_DIR}/sys.log 2>&1 < /dev/null  &
+      nohup nice -n $JOBKEEPER_NICENESS  ${JAVA} ${JAVA_ARGS} -classpath $CLASSPATH ${MAIN_CLASS} ${JOBKEEPER_ID}  $@>>${LOGS_DIR}/sys.log 2>&1 < /dev/null  &
   else
       echo "Jobkeeper is started ,you need to stop it!!!"
       exit 1;
